f3677e3bd2db56cbbb9e9aefedb83c7630863672
stein-rip <stein@stein.rip>
2024-03-02 14:05:23 -0500

    backend storing data

--- functions/src/models/Favorite.ts
+++ functions/src/models/Favorite.ts
@@ -2,11 +2,12 @@ import { ObjectId } from "mongodb";
 
 interface Cat {
   id: string;
-    url: string;
+  url: string;
+  catName: string;
 }
 
 export default interface Favorite {
-  _id: ObjectId;
+  _id?: ObjectId;
   cat: Cat;
   userId?: string;
 }
